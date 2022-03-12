6e490f219f5a70a20091c094859b3e76788ebcaa
generoso <generoso.pagano@gmail.com>
2022-03-12 12:04:19 +0100

    envs

--- modules/shell/aliases/envs.sh
+++ modules/shell/aliases/envs.sh
@@ -3,20 +3,6 @@
 envs_dir=~/.venvs/
 env_file=.venv
 
-## Create env
-
-create_env() {
-    cd $envs_dir
-    python3 -m venv $1
-    cd - > /dev/null
-}
-
-## Activate env
-
-activate_env() {
-    source ${envs_dir}/$1/bin/activate
-}
-
 ## Autocompletion
 
 _envs() {
@@ -27,7 +13,35 @@ _envs() {
     return 0
 }
 
-complete -F _envs activate_env
+## Create env
+
+env_create() {
+    mkdir -p $envs_dir
+    cd $envs_dir
+    python3 -m venv $1
+    cd - > /dev/null
+}
+
+## Activate env
+
+env_activate() {
+    source ${envs_dir}/$1/bin/activate
+}
+
+complete -F _envs env_activate
+
+## Deactivate env
+
+alias env_deactivate=deactivate
+
+## Install env for auto activation
+
+env_install() {
+    local env=$1
+    echo $env > $env_file
+}
+
+complete -F _envs env_install
 
 ## Activate when entering a directory with .venv file
 
@@ -36,15 +50,6 @@ PROMPT_COMMAND='prompt'
 prompt() {
     if [ "$PWD" != "$MYOLDPWD" ]; then
         MYOLDPWD="$PWD"
-        test -e $env_file && activate_env $(cat $env_file)
+        test -e $env_file && env_activate $(cat $env_file)
     fi
 }
-
-## Install env for auto activation
-
-install_env() {
-    local env=$1
-    echo $env > $env_file
-}
-
-complete -F _envs install_env
